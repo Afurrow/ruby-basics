d389ba7e47782a6fdc02b733acaf9dd807622a80
AFurrow <austindfurrow@gmail.com>
2020-09-04 20:32:15 -0600

    Finished stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,5 +1,21 @@
-def stock_picker (days) 
+require 'pry'
+
+def stock_picker (arr) 
+bestArr = []
+high = 0
+
+arr.each_with_index do |num1, x|
+  arr.each_with_index do |num2, y|
+    if num2 - num1 > high && y > x
+      high = num2 - num1
+      bestArr = [x, y] 
+      # binding.pry
+    end
+  end
+end
+
+bestArr
 
 end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17,3,6,9,15,8,6,1,10])
